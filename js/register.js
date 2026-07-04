d3b019216d5a66256c10d511a4ca7c3d32c65708
manjot5857-cloud <manjot15857@gmail.com>
2026-07-03 18:44:21 -0700

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,5 +1,76 @@
-document.getElementById("registerBtn").addEventListener("click", () => {
+const registerBtn = document.getElementById("registerBtn");
 
-    alert("Register API coming next!");
+registerBtn.addEventListener("click", register);
 
-});
+async function register() {
+
+    const fullName = document.getElementById("fullname").value.trim();
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    const message = document.getElementById("message");
+
+    message.innerHTML = "";
+
+    if (!fullName || !username || !email || !password) {
+
+        message.innerHTML = "Please fill all fields.";
+
+        return;
+    }
+
+    if (password !== confirmPassword) {
+
+        message.innerHTML = "Passwords do not match.";
+
+        return;
+    }
+
+    try {
+
+        const response = await fetch(API_URL + "/auth/register", {
+
+            method: "POST",
+
+            headers: {
+
+                "Content-Type": "application/json"
+
+            },
+
+            body: JSON.stringify({
+
+                fullName,
+                username,
+                email,
+                password
+
+            })
+
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+
+            message.innerHTML = data.message;
+
+            return;
+
+        }
+
+        alert("Registration Successful!");
+
+        window.location.href = "login.html";
+
+    }
+
+    catch {
+
+        message.innerHTML = "Unable to connect to server.";
+
+    }
+
+}
