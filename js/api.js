ec333fa9a00e606272685ad5722bb329bfc12b1b
manjot5857-cloud <manjot15857@gmail.com>
2026-07-04 00:52:59 -0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,49 @@
 const API_URL = "https://api.fastedgehosting.com/api";
+
+const api = {
+
+    async get(endpoint) {
+
+        const response = await fetch(API_URL + endpoint, {
+
+            headers: {
+
+                "Authorization": "Bearer " + localStorage.getItem("token")
+
+            }
+
+        });
+
+        if (!response.ok)
+            throw new Error(await response.text());
+
+        return await response.json();
+
+    },
+
+    async post(endpoint, body) {
+
+        const response = await fetch(API_URL + endpoint, {
+
+            method: "POST",
+
+            headers: {
+
+                "Content-Type": "application/json",
+
+                "Authorization": "Bearer " + localStorage.getItem("token")
+
+            },
+
+            body: JSON.stringify(body)
+
+        });
+
+        if (!response.ok)
+            throw new Error(await response.text());
+
+        return await response.json();
+
+    }
+
+};
